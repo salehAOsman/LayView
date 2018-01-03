f4de045dc60cd0d6ef9f069f6bf79ef597d7b4c7
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-03 11:42:00 +0100

    Add project files.

---
+++ LayView/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Get: Index
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        // GET: About
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        // Get: Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Name = "Saleh Osman";
+            ViewBag.Phone = "0764–191954";
+            ViewBag.ePost = "Mail:saleh1.a.osman@gmail.com";
+            ViewBag.Address = "Kronotorpsvägen3C,37161Lyckeby";
+
+            return View();
+        }
+        // Get: Projects
+        public ActionResult Projects()
+        {
+            ViewBag.projects = "Projects I have finished:";
+            return View();
+        }
+    }
+}
